3d2d0e55f233657f33fb6e45db8340b251d3ce97
Dan Vitoriano <vitoriano08@gmail.com>
2024-11-27 09:17:10 -0300

    08 - add categories screen with grid layout and category constants

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture_application/utils/const/categories.dart';
 
 class Categories extends StatelessWidget {
   const Categories({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      appBar: AppBar(title: const Text('Categories')),
-      body: const Center(child: Text('Categories')),
-    ));
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              title: const Text('Escolha uma categoria'),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.bookmark),
+                  onPressed: () {
+                    // Navigate to the search screen
+                  },
+                ),
+              ],
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(8),
+              child: GridView(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 8,
+                    mainAxisExtent: 200,
+                  ),
+                  children: 
+                    categories.keys.map((e) => Text("Category: ${categories[e]}")).toList(),
+                  ),
+            )));
   }
 }
---
+++ lib/utils/const/categories.dart
@@ -0,0 +1,8 @@
+const String imagePath = "assets/images/";
+
+const Map<String, String> categories = {
+  'jewelery': 'Jóias',
+  'electronics': 'Eletrônicos',
+  'men\'s clothing': 'Roupas Masculinas',
+  'women\'s clothing': 'Roupas Femininas',
+};
